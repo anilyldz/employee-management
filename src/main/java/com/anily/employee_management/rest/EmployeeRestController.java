126c003e145d1ae8ad2f51b6e91346bb47e434b9
anilyildiz <anil-yildiz@outlook.com.tr>
2024-06-14 21:02:46 +0300

    EmployeeRestController added

---
+++ src/main/java/com/anily/employee_management/rest/EmployeeRestController.java
@@ -0,0 +1,26 @@
+package com.anily.employee_management.rest;
+
+import com.anily.employee_management.dao.EmployeeDAO;
+import com.anily.employee_management.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+}
